5a475362e8bab1262729ae3dd4f38471fc08a610
dagg0027 <32076485+dagg0027@users.noreply.github.com>
2017-11-26 23:36:47 -0500

    added javascript

--- gulpfile.js
+++ gulpfile.js
@@ -30,3 +30,101 @@ gulp.task('default', function () {
     sassPath, // which files to watch for changes (use our variable)
     ['sass']) // an array of tasks to run when changes are detected.
 })
+
+// JavaScript Document //
+window.onload = buildOverlay;
+
+
+function buildOverlay() {
+
+
+// created a div to use as an overlay
+    overlay = document.createElement("div");
+// Added an id of overlay
+    overlay.setAttribute("id","overlay");
+
+
+// created a div to use as the x button
+    equis = document.createElement("div");
+// Added an id of x
+    equis.setAttribute("id","equis");
+
+// created a img
+    leimage = document.createElement("img");
+
+    getThumbnails();
+
+    equis.innerHTML = "<p id=closebutton>X</p>"
+}
+
+
+function getThumbnails() {
+
+    // added an event listener
+    gallery.addEventListener("click", function(e) {
+
+    // if an image is clicked on
+    if (e.target.nodeName == "IMG"){
+    // get source of image
+    let imgsrc = e.target.getAttribute("src");
+
+
+    // replaced thumbnail with large
+    changedsrc = imgsrc.replace("thumbnail", "large");
+
+    addOverlay();
+
+    }
+});
+
+}
+
+function addOverlay(){
+
+
+    // added overlay to body
+
+    document.body.appendChild(overlay);
+
+    // Give my image the new changedsrc
+
+    leimage.setAttribute("src", changedsrc);
+
+    // added leimage to overlay
+
+    overlay.appendChild(leimage);
+
+
+    // added x to overlay
+    overlay.appendChild(equis);
+
+   Close();
+}
+
+
+
+function Close(){
+
+    // created keydown action to close image
+
+    document.addEventListener("keydown", function(e){
+        if (e.which == 27){
+
+         overlay.remove("visible");
+            }
+                           });
+
+    // created closebutton action
+
+     overlay.addEventListener("click", function(e) {
+        if (e.target.id == 'closebutton') {
+
+            overlay.remove("visible");
+
+        }
+    });
+
+
+    getThumbnails();
+}
+
